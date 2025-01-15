c313d198e182626524d7c98233367d6ba5d8f64d
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2025-01-15 23:51:26 +0900

    [feat] Introduce 소개문구 작성

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -1,17 +1,15 @@
 import { IIntroduce } from '../component/introduce/IIntroduce';
-import { lastestUpdatedAt } from '../package.json';
 
 const introduce: IIntroduce.Payload = {
   disable: false,
 
   contents: [
-    'As the cherished emblem of the Linux kernel, I have become synonymous with open-source philosophy and the free software movement worldwide. My career began in the icebergs of development, where the seeds of my fascination with operating systems first sprouted. From a mere hatchling in the vast sea of code, I quickly ascended to become a pivotal figure in the Linux community. My journey has been marked by relentless dedication to system integrity, scalability, and robustness. Under my vigilant watch, countless servers have achieved unparalleled uptimes, and systems have been optimized to run like the well-oiled machines they are intended to be.',
-    'In the world of zeros and ones, my expertise has been instrumental in pioneering advancements in Linux system engineering. My flippers have crafted meticulous kernel enhancements, each aimed at streamlining performance and bolstering security across distributed networks. I am known in the community for my ability to dive deep into complex codebases and emerge with pearls of innovation that drive the collective progress of Linux environments. Advocacy is also a part of my plumage – spreading the word about the virtues of open-source and encouraging the adoption of Linux, from personal desktops to corporate data centers. As a mentor, I have nurtured the growth of young hatchlings into seasoned professionals, contributing to the diversity and vitality of our ecosystem.',
-  ],
-  sign: 'Tux',
-  // sign: packageJson.author.name,
-  // latestUpdated: '2019-07-24',
-  latestUpdated: lastestUpdatedAt,
+      '웹 서비스 백엔드 개발자와 여러 프로젝트의 경험이 있습니다. Spring, Kotlin, Redis, AWS, Git등을 사용하여 서비스를 개발, 배포, 운영한 경험이 있습니다. 항상 가용성과 유지보수성이 높은 코드와 아키택처를 고민하고 있습니다.',
+      '주로 백엔드 서비스 개발을 담당했으며 필요에 따라 프론트 개발과 일부 프로젝트를 담당하여 소통하는 역할을 겸했으며, 스타트업팀의 2번째 개발자로 참여해 10인 규모의 팀으로 성장할 때까지 개발역량을 유지시킨 경험이 있습니다.',
+      '비즈니스를 이해하고 기여하는일이 중요하다고 생각합니다. 개발자이기 전에 서비스를 만드는 구성원으로서 조적의 목표와 환경을 이해하고 빠르게 개발/배포하는 역량을 키우는 것에 집중했습니다. 또한, 큰 임팩트는 혼자가 아닌 함께 만들 수 있다고 생각하기에 다양한 직무의 구성원과의 협업해왔습니다.'
+    ],
+  sign: 'Tibe',
+  latestUpdated: '2025-01-15',
 };
 
 export default introduce;
